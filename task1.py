ac7d60a4199b0130eb8cb5678d839d0fd75801b6
Ivan <kamenev2003kamenev@gmail.com>
2025-09-11 13:30:45 +0700

    task1 first version

--- task1.py
+++ task1.py
@@ -1,1 +1,16 @@
-#This is 1 task
+import cv2
+import numpy as np
+
+image = np.full((100, 100, 4), (255, 255, 255, 255), dtype=np.uint8)
+
+mask = np.zeros((100, 100), dtype=np.uint8)
+center = (50, 50)  
+radius = 40  
+cv2.circle(mask, center, radius, 255, -1)  
+
+circle_image = np.zeros((100, 100, 4), dtype=np.uint8)
+cv2.circle(circle_image, center, radius, (0, 0, 255, 128), -1)
+
+image[mask > 0] = circle_image[mask > 0]
+
+cv2.imwrite('red_circle.png', image)
